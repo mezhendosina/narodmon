35bd573cb036a2ace2741eaad112ab4017e51f1e
DarkCat09 <a.chechkenev@yandex.ru>
2023-06-05 18:41:02 +0400

    Почищены импорты, hadlineText->Content (иначе не билдится)

--- app/src/main/java/ru/nm17/narodmon/ui/elements/Scaffolds.kt
+++ app/src/main/java/ru/nm17/narodmon/ui/elements/Scaffolds.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
 
