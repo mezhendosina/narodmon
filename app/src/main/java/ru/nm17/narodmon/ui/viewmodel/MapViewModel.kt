266e2001f2f8fc79984ed184dc6edcc104c8ab9a
DarkCat09 <a.chechkenev@yandex.ru>
2023-06-05 18:59:23 +0400

    Нерабочий фикс карты

--- app/src/main/java/ru/nm17/narodmon/ui/viewmodel/MapViewModel.kt
+++ app/src/main/java/ru/nm17/narodmon/ui/viewmodel/MapViewModel.kt
@@ -10,7 +10,8 @@ import java.net.URL
 
 class MapViewModel : ViewModel() {
     private val tileStreamProvider = TileStreamProvider { row, col, zoom ->
-        URL("https://tile.openstreetmap.org/${zoom}/${row}/${col}").openStream()
+        URL("https://tile.openstreetmap.org/${zoom}/${row}/${col}.png").openStream()
+        //URL("https://tile2.maps.2gis.com/poi?x=${row}&y=${col}&z=${zoom}&v=1&ts=online_sd").openStream()
     }
 
     val state: MapState by mutableStateOf(
