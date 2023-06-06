0935cd88d434f26fe958aebf237e65ea703b7b8c
DarkCat09 <a.chechkenev@yandex.ru>
2023-06-06 14:57:49 +0400

    Перенёс карту в отдельный компонент вместе со state

--- app/src/main/java/ru/nm17/narodmon/ui/pages/Sensors.kt
+++ app/src/main/java/ru/nm17/narodmon/ui/pages/Sensors.kt
@@ -9,23 +9,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import ovh.plrapps.mapcompose.api.scale
-import ovh.plrapps.mapcompose.api.setScroll
-import ovh.plrapps.mapcompose.ui.MapUI
 import ru.nm17.narodmon.R
 import ru.nm17.narodmon.ui.elements.GenericNavScaffold
-import ru.nm17.narodmon.ui.viewmodel.MapViewModel
+import ru.nm17.narodmon.ui.elements.TileMap
 
 enum class SensorsFilter {
     All, Thermometer, Camera,
@@ -34,24 +29,17 @@ enum class SensorsFilter {
 @ExperimentalMaterial3Api
 @Composable
 fun SensorsPage(navController: NavController) {
-    val mapVM by remember { mutableStateOf(MapViewModel()) }
     var filter by remember { mutableStateOf(SensorsFilter.All) }
 
     val scrConfig = LocalConfiguration.current
     val mapHeight = scrConfig.screenHeightDp / 3
-    
-    LaunchedEffect(mapVM) {
-        // TODO: Подгружать сохранённую позицию
-        mapVM.state.setScroll(Offset(28702.6F, 14787.6F))
-        mapVM.state.scale = 1.4658884F
-    }
 
     GenericNavScaffold(
         title = { Text(text = stringResource(R.string.sensors_page_title)) }
     ) {
         Column(modifier = Modifier.padding(it)) {
 
-            MapUI(state = mapVM.state, modifier = Modifier.height(mapHeight.dp))
+            TileMap(modifier = Modifier.height(mapHeight.dp))
 
             Row(
                 modifier = Modifier.padding(horizontal = 8.dp),
