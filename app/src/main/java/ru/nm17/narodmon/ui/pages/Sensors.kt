3a873725f289fdee4b0cac85dc2e71d174cf29f5
DarkCat09 <a.chechkenev@yandex.ru>
2023-06-06 17:12:20 +0400

    Добавлен BottomSheet для фильтров и строки

--- app/src/main/java/ru/nm17/narodmon/ui/pages/Sensors.kt
+++ app/src/main/java/ru/nm17/narodmon/ui/pages/Sensors.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,6 +35,35 @@ enum class SensorsFilter {
 fun SensorsPage(navController: NavController) {
     var searchQuery by remember { mutableStateOf("") }
     var searchActive by remember { mutableStateOf(false) }
+
+    var filterShown by remember { mutableStateOf(false) }
+    var filterMine by remember { mutableStateOf(false) }
+
+    val filterItems = listOf<String>(
+        stringResource(R.string.filter_temp),
+        stringResource(R.string.filter_temp_water),
+        stringResource(R.string.filter_temp_ground),
+        stringResource(R.string.filter_temp_dew_point),
+        stringResource(R.string.filter_humidity),
+        stringResource(R.string.filter_pressure),
+        stringResource(R.string.filter_lightness),
+        stringResource(R.string.filter_uv),
+        stringResource(R.string.filter_radiation),
+        stringResource(R.string.filter_rainfall),
+        stringResource(R.string.filter_dust),
+        stringResource(R.string.filter_wind_speed),
+        stringResource(R.string.filter_wind_direction),
+        stringResource(R.string.filter_concentration),
+        stringResource(R.string.filter_power),
+        stringResource(R.string.filter_voltage),
+        stringResource(R.string.filter_amperage),
+        stringResource(R.string.filter_energy),
+        stringResource(R.string.filter_battery),
+        stringResource(R.string.filter_rxtx),
+        stringResource(R.string.filter_signal),
+        stringResource(R.string.filter_water_meter),
+        stringResource(R.string.filter_time),
+    )
 
     val scrConfig = LocalConfiguration.current
     val mapHeight = scrConfig.screenHeightDp / 3
@@ -61,10 +92,27 @@ fun SensorsPage(navController: NavController) {
                 modifier = Modifier.padding(horizontal = 8.dp),
             ) {
                 AssistChip(
-                    onClick = { },
+                    onClick = { filterShown = true },
                     label = { Text(text = stringResource(R.string.sensors_filter)) },
                 )
+
+                AssistChip(
+                    onClick = { },
+                    label = { Text(text = stringResource(R.string.sensors_sorting)) },
+                )
+
+                FilterChip(
+                    selected = filterMine,
+                    onClick = { filterMine = !filterMine },
+                    label = { Text(text = stringResource(R.string.sensors_mine)) },
+                )
             }
+        }
+    }
+
+    if (filterShown) {
+        ModalBottomSheet(onDismissRequest = { filterShown = false }) {
+            Text(text = "Hello")
         }
     }
 }
