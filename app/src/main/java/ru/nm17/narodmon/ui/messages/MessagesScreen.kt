628b168bcfd0d940bc1bfdb69616dfd2d9b37930
mezhendosina <mensh.zheya@gmail.com>
2023-06-21 14:28:26 +0500

    Заглушки для SettingsScreen и MessagesScreen

---
+++ app/src/main/java/ru/nm17/narodmon/ui/messages/MessagesScreen.kt
@@ -0,0 +1,19 @@
+package ru.nm17.narodmon.ui.messages
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MessagesScreen() {
+    Text(text = "todo")
+}
+
+@Preview
+@Composable
+fun PreviewMessagesScreen() {
+    MessagesScreen()
+}
---
+++ app/src/main/java/ru/nm17/narodmon/ui/settings/SettingsScreen.kt
@@ -0,0 +1,16 @@
+package ru.nm17.narodmon.ui.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "todo")
+}
+
+@Preview
+@Composable
+fun PreviewSettingsScreen() {
+    SettingsScreen()
+}
