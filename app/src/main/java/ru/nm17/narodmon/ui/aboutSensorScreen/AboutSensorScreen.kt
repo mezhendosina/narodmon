bb2709e5260f4acad2eb4cb5b1962174d4a28dd8
mezhendosina <mensh.zheya@gmail.com>
2023-08-20 11:51:06 +0500

    create AboutSensorScreen.kt

---
+++ app/src/main/java/ru/nm17/narodmon/ui/aboutSensorScreen/AboutSensorScreen.kt
@@ -0,0 +1,15 @@
+package ru.nm17.narodmon.ui.aboutSensorScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AboutSensorScreen() {
+
+}
+
+@Preview
+@Composable
+fun AboutSensorScreenPreview() {
+
+}
