90cdc3f71d1a4cf13d89b464c9c7686642ddd263
mezhendosina <mensh.zheya@gmail.com>
2023-08-22 13:02:08 +0500

    create AboutSensor ui

--- app/src/main/java/ru/nm17/narodmon/ui/aboutSensorScreen/AboutSensorScreen.kt
+++ app/src/main/java/ru/nm17/narodmon/ui/aboutSensorScreen/AboutSensorScreen.kt
@@ -1,15 +1,33 @@
 package ru.nm17.narodmon.ui.aboutSensorScreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ru.nm17.narodmon.ui.entities.AboutSensorUiEntity
+import ru.nm17.narodmon.ui.theme.NarodMonTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutSensorScreen() {
-
+fun AboutSensorScreen(aboutSensorUiEntity: AboutSensorUiEntity) {
+    Scaffold(topBar = {
+        LargeTopAppBar(title = { Text(aboutSensorUiEntity.name) })
+    }) {
+        Column(Modifier.padding(it)) {
+            
+        }
+    }
 }
 
 @Preview
 @Composable
 fun AboutSensorScreenPreview() {
-
+    NarodMonTheme {
+        AboutSensorScreen(aboutSensorUiEntity = AboutSensorUiEntity("123", emptyList()))
+    }
 }
